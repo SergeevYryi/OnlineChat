c249c32896495d30ae525ee68aeb8349e7c46535
SergeevYryi <sergeev.y90@inbox.ru>
2021-06-07 20:31:00 +0400

    work 4

--- client/src/Chat/Client/ClientWindow.java
+++ client/src/Chat/Client/ClientWindow.java
@@ -1,7 +1,36 @@
 package Chat.Client;
 
-public class ClientWindow {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
 
+public class ClientWindow extends JFrame {
+    private static final String IP_ADDR = "192.168.31.177";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("Yuri");
+    private final JTextField fieldInput = new JTextField();
+
+
+    private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+        setVisible(true);
     }
 }
