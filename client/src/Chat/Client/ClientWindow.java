e1c58835d2a44b6cab63dae8f5d4e6c350943277
SergeevYryi <sergeev.y90@inbox.ru>
2021-06-08 12:53:10 +0400

    work 6

--- client/src/Chat/Client/ClientWindow.java
+++ client/src/Chat/Client/ClientWindow.java
@@ -1,9 +1,15 @@
 package Chat.Client;
 
+import network.TCPConnection;
+import network.TCPConnectionListener;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
-public class ClientWindow extends JFrame {
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
     private static final String IP_ADDR = "192.168.31.177";
     private static final int PORT = 8189;
     private static final int WIDTH = 600;
@@ -21,16 +27,61 @@ public class ClientWindow extends JFrame {
     private final JTextField fieldNickname = new JTextField("Yuri");
     private final JTextField fieldInput = new JTextField();
 
-
+    private TCPConnection connection;
     private ClientWindow() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+
         log.setEditable(false);
         log.setLineWrap(true);
         add(log, BorderLayout.CENTER);
+
+        fieldInput.addActionListener(this);
         add(fieldInput, BorderLayout.SOUTH);
         add(fieldNickname, BorderLayout.NORTH);
+
         setVisible(true);
+
+            connection = new TCPConnection();  // от сюда
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String msg = fieldInput.getText();
+        if(msg.equals("")) return;
+        fieldInput.setText(null);
+        connection.sendString(fieldNickname.getText() + ": " + msg);
+    }
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMsg("Connection ready!");
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+        printMsg(value);
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMsg("Connection close!");
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMsg("Connection exception: " + e);
+    }
+    private synchronized void printMsg (String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
     }
 }
